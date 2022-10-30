21a0d5af6ee0a82f19a76030e61ca204290da3bc
Krish Sadhwani <92883393+KrishT97@users.noreply.github.com>
2022-10-30 20:18:11 +0000

    Update FileChecker.java

--- Project/main/java/FileChecker.java
+++ Project/main/java/FileChecker.java
@@ -2,7 +2,7 @@ import java.io.File;
 
 public class FileChecker {
 
-    public static boolean check(File folder, File file){
+    public static boolean check(File file, File folder){
         File jsonFile = new File(folder + "\\" + file.getName().replace(".txt", "") + ".json");
         if (!jsonFile.exists()){
             return file.getName().endsWith(".txt");
