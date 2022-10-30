dc42abfb2db6d601cf4d95b700326d2c21ccdd10
Krish Sadhwani <92883393+KrishT97@users.noreply.github.com>
2022-10-30 20:17:45 +0000

    Update FileGenerator.java

--- Project/main/java/FileGenerator.java
+++ Project/main/java/FileGenerator.java
@@ -14,7 +14,7 @@ public class FileGenerator {
 
         assert listOfFiles != null;
         for (File file : listOfFiles) {
-            if (file.isFile() && FileChecker.check(folder, file)) {
+            if (file.isFile() && FileChecker.check(file, folder)) {
 
                BufferedReader br = FileCollector.read(file, folder);
 
