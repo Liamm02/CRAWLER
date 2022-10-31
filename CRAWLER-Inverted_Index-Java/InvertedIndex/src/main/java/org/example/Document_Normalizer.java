3b2a72983253cb0bfbe40ddcd419452589c4af3d
Liamm02 <90764145+Liamm02@users.noreply.github.com>
2022-10-31 16:55:02 +0000

    Inverted Index in Java

---
+++ CRAWLER-Inverted_Index-Java/InvertedIndex/src/main/java/org/example/Document_Normalizer.java
@@ -0,0 +1,66 @@
+package org.example;
+
+import java.io.*;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.*;
+
+public class Document_Normalizer {
+
+    String path;
+    public Document_Normalizer(String path){
+        this.path = path;
+    }
+
+    public List<String> Translator(String doc) throws IOException {
+        String text = new String(Files.readAllBytes(Paths.get(this.path+"Documents/"+ doc)));
+
+
+        String lines = text.replaceAll("\\p{Punct}|[,‘’×·”“—«»—ºªπ~:+\\[\\\\@^{%(-*|&<`}._=\\]!>;?¿¡#$)/]", " ").toLowerCase();
+        String Fixedlines = lines.trim().replaceAll(" +", " ");
+        String[] linesArray = Fixedlines.split("\\r?\\n");
+        List<String> LinesList = new ArrayList<String>(Arrays.asList(linesArray));
+        return LinesList;
+    }
+
+    public HashMap<String, Set<Integer>> Stopwords_Deleter(List<String> LinesList,String Language) throws IOException {
+
+        String StopwordsText = new String(Files.readAllBytes(Paths.get(
+                this.path+"Stopwords/"+Language+"_stopwords.txt")));
+        String[] stopwords = StopwordsText.split(",");
+        Set<String> StopwordsSet = new HashSet<String>(Arrays.asList(stopwords));
+        HashMap<String, Set<Integer>> map = new HashMap<String, Set<Integer>>();
+        int line = 1;
+        for (String cadena: LinesList) {
+            if(!cadena.isBlank() | !cadena.isEmpty()) {
+
+                Set<Integer> temp = new HashSet<Integer>();
+                String[] words = cadena.split(" ");
+                List<String> wordsList = new ArrayList<String>(Arrays.asList(words));
+
+                for (String word : wordsList) {
+                    if (word != "" && !word.isBlank()) {
+                        if (StopwordsSet.contains(word)) {
+                        } else {
+                            if (!map.containsKey(word)) {
+                                temp.add(line);
+                                Set<Integer> temp2 = new HashSet<Integer>();
+                                temp2.addAll(temp);
+                                map.put(word, temp2);
+                                temp.clear();
+                            }
+                            else if (map.containsKey(word)) {
+                                Set<Integer> temp3 = map.get(word);
+                                temp3.add(line);
+                                map.replace(word, temp3);
+                            }
+                        }
+                    }
+
+                }
+            }
+            line += 1;
+        }
+        return map;
+    }
+}
---
+++ CRAWLER-Inverted_Index-Java/InvertedIndex/src/main/java/org/example/Main.java
@@ -0,0 +1,26 @@
+package org.example;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws Exception {
+        String path = "./src/main/resources/";
+        Checker checker = new Checker(path);
+        ArrayList<String> documents = checker.documents_checker();
+        checker.MetaData_checker();
+        checker.Inv_Index_checker();
+
+        for (String name: documents){
+            System.out.println("Indexing document: "+name);
+            Document doc = new Document(name,path);
+            doc.ReadMetada();
+
+            Document_Normalizer dn = new Document_Normalizer(path);
+            List<String> text = dn.Translator(name);
+            HashMap<String, Set<Integer>> Doc_InvertedIndex = dn.Stopwords_Deleter(text,doc.Language);
+
+            Document_indexer di = new Document_indexer();
+            di.Indexer(Doc_InvertedIndex,name,path);
+
+        }
+    }
+}
