dfdbefaa55312644ac5db61fcc6241d286d6d783
Liamm02 <90764145+Liamm02@users.noreply.github.com>
2022-10-31 20:45:15 +0000

    Crawler final version

--- Crawler/src/main/java/Crawler_Package/Main.java
+++ Crawler/src/main/java/Crawler_Package/Main.java
@@ -1,19 +1,14 @@
 package Crawler_Package;
 
-import Downloader.Checker;
-import Downloader.Searcher;
-
-import java.io.File;
-
 public class Main {
 
     public static void main(String[] args) throws Exception {
         String path = ".\\src\\main\\resources";
-        Properties_Editor pe = new Properties_Editor(path);
-        int n = Integer.valueOf(pe.Get_Propertie("LastDownloaded")) + 1;
-        System.out.println("Starting search by: " + n +"\n");
+        PropertiesEditor pe = new PropertiesEditor(path);
+        int SearchIDStarter = Integer.valueOf(pe.GetProperties("LastDownloaded")) + 1;
+        System.out.println("Starting search by: " + SearchIDStarter +"\n");
         System.out.println("-------------------------------------------------------------------------------------");
-        TimeTask_Gutenberg gt = new TimeTask_Gutenberg(path);
+        TimeTaskGutenberg gt = new TimeTaskGutenberg(path);
         gt.start();
 
 
