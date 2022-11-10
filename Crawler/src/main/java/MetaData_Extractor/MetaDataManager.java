8c6b147a79af84d9ec0ace8b0eb9e0839a6ae89a
Liamm02 <90764145+Liamm02@users.noreply.github.com>
2022-11-10 11:51:14 +0000

    Update MetaDataManager.java

--- Crawler/src/main/java/MetaData_Extractor/MetaDataManager.java
+++ Crawler/src/main/java/MetaData_Extractor/MetaDataManager.java
@@ -9,9 +9,7 @@ public class MetaDataManager {
 
     }
     public static void modify(LinkedHashMap<String, String> map, String lastKey, String lastValue, String jsonValue){
-        //System.out.println(lastKey + lastValue+ jsonValue);
         map.replace(lastKey.trim(), lastValue.trim(), lastValue.trim() + " " + jsonValue.trim());
-        //System.out.println(map);
     }
 
 }
